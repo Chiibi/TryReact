46ad8039014c99151de29c0b8656d2b096fc70f7
Chiibi <Chiibi_jame@hotmail.com>
2018-03-08 00:02:27 +0700

    [Update] Burger component

--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.jsx
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.jsx
@@ -3,39 +3,38 @@ import './BurgerIngredient.css'
 import PropTypes from 'prop-types'
 
 class BurgerIngredient extends Component {
-    render() {
-      return 
-        let ingredient = null
+	render() {
+		let ingredient = null
 
-        switch (props.type) {
-            case ('bread-bottom'):
-                ingredient = <div className="BreadBottom" ></div>
-                break
-            case ('bread-top'):
-                ingredient = (
-                    <div className="BreadTop">
-                        <div classNae="Seeds1"></div>
-                        <div classNae="Seeds2"></div>
-                    </div> 
-                )
-                break
-            case ('meat'):
-                ingredient = <div className="Meat" ></div>
-                break
-            case ('cheese'):
-                ingredient = <div className="Cheese" ></div>
-                break
-            case ('bacon'):
-                ingredient = <div className="Bacon" ></div>
-                break
-            case ('salad'):
-                ingredient = <div className="Salad" ></div>
-                break
-            default: null
-        }
+		switch (this.props.type) {
+				case ('bread-bottom'):
+						ingredient = <div className="BreadBottom" ></div>
+						break
+				case ('bread-top'):
+						ingredient = (
+								<div className="BreadTop">
+										<div className="Seeds1"></div>
+										<div className="Seeds2"></div>
+								</div> 
+						)
+						break
+				case ('meat'):
+						ingredient = <div className="Meat" ></div>
+						break
+				case ('cheese'):
+						ingredient = <div className="Cheese" ></div>
+						break
+				case ('bacon'):
+						ingredient = <div className="Bacon" ></div>
+						break
+				case ('salad'):
+						ingredient = <div className="Salad" ></div>
+						break
+				default: ingredient = null
+		}
 
-        return ingredient   
-    }
+		return ingredient   
+	}
 }
 
 BurgerIngredient.propTypes = {
